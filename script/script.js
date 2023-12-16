f1bac403a847f59f5d4503a75af28ab80eabf57f
Aleksejs Šepeļevs <118129406+AleksejsGIT@users.noreply.github.com>
2023-12-16 15:33:53 +0200

    brrrr

--- script/script.js
+++ script/script.js
@@ -1,71 +1,90 @@
 function startTimer(duration, display) {
-    var start = Date.now(),
-        diff,
-        minutes,
-        seconds;
-    function timer() {
-      diff = duration - (((Date.now() - start) / 1000) | 0);
-      minutes = (diff / 60) | 0;
-      seconds = (diff % 60) | 0;
-  
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-  
-      display.textContent = minutes + ":" + seconds;
-  
-      if (diff <= 0) {
-        start = Date.now() + 1000;
-      }
-    };
-    timer();
-    setInterval(timer, 1000);
+  var start = Date.now(),
+      diff,
+      minutes,
+      seconds;
+  function timer() {
+    diff = duration - (((Date.now() - start) / 1000) | 0);
+    minutes = (diff / 60) | 0;
+    seconds = (diff % 60) | 0;
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (diff <= 0) {
+      start = Date.now() + 1000;
+    }
   }
-  
-  function displayInfo() {
-    let now = new Date();
-  
-    let hours = now.getHours().toString().padStart(2, '0');
-    let minutes = now.getMinutes().toString().padStart(2, '0');
-    let time = hours + ":" + minutes;
-  
-    let day = now.getDate().toString().padStart(2, '0');
-    let month = (now.getMonth() + 1).toString().padStart(2, '0');
-    let year = now.getFullYear().toString().substr(-2);
-    let currentDate = `${day}.${month}.${year}`;
-  
-    document.getElementById('time').textContent = time;
-    document.getElementById('date').textContent = currentDate;
-  }
-  
-  // This should be at the end of your script.js file
+  timer();
+  setInterval(timer, 1000);
+}
+
+function displayInfo() {
+  let now = new Date();
+
+  let hours = now.getHours().toString().padStart(2, '0');
+  let minutes = now.getMinutes().toString().padStart(2, '0');
+  let time = hours + ":" + minutes;
+
+  let day = now.getDate().toString().padStart(2, '0');
+  let month = (now.getMonth() + 1).toString().padStart(2, '0');
+  let year = now.getFullYear().toString().substr(-2);
+  let currentDate = `${day}.${month}.${year}`;
+
+  document.getElementById('time').textContent = time;
+  document.getElementById('date').textContent = currentDate;
+  // Also update the modal information when displayInfo is called
+  document.getElementById('modalTime').textContent = time;
+  document.getElementById('modalDate').textContent = currentDate;
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
   var fiveMinutes = 60 * 10,
       display = document.querySelector('#timer');
   startTimer(fiveMinutes, display);
-  displayInfo();
+  displayInfo(); // Call displayInfo here to update the time and date on load
 
   var modal = document.getElementById("modal");
   var btn = document.getElementById("triggerModal");
   var closeModal = document.getElementById('closeModal');
 
-  // Assuming you want to blur the .squareTop and .squareBot
-  var blurElements = document.querySelectorAll('.squareTop, .squareBot');
-
+  // Open the modal and copy the information
   btn.onclick = function() {
+    displayInfo(); // Update the info in the modal
     modal.style.display = "block";
-    blurElements.forEach(el => el.style.filter = "blur(5px)");
+    
   }
 
+  // Close the modal
   closeModal.onclick = function() {
     modal.style.display = "none";
-    blurElements.forEach(el => el.style.filter = "none");
+    document.body.style.filter = "none";
   }
 
   // Clicking outside the modal closes it
   window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
-      blurElements.forEach(el => el.style.filter = "none");
+      document.body.style.filter = "none";
     }
   }
 });
+
+function updateModalCurrentTime() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+  const currentTimeFormatted = `${hours}:${minutes}:${seconds}`;
+
+  document.getElementById('modalCurrTime').textContent = currentTimeFormatted;
+}
+
+// Update the modal current time every second
+setInterval(updateModalCurrentTime, 1000);
+
+
+// Rest of your JavaScript...
+
